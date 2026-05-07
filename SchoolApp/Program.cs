0219eeb9036b071f84fd2201aa3996cf01747091
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-05-07 20:05:55 +0300

    feat: add migration, docker setup and db seed

--- SchoolApp/Program.cs
+++ SchoolApp/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace SchoolApp
 {
     public class Program
@@ -7,19 +9,19 @@ namespace SchoolApp
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var conString = builder.Configuration.GetConnectionString("DevConnection");
+
+            builder.Services.AddDbContext<Data.SchoolMvc9Context>(options =>
+                options.UseSqlServer(conString));
+
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
+            
 
             app.UseHttpsRedirection();
 
